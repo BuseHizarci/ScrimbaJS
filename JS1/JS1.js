367228407fe0b0546e28d273324d21dd9ccb0192
Buse HIZARCI <bhzrc2705@gmail.com>
2022-08-10 22:09:52 +0100

    Something went wrong msg

--- JS1/JS1.js
+++ JS1/JS1.js
@@ -2,8 +2,10 @@
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
 
-
-
+let purchaseParagrapgh = document.getElementById("error")
+console.log(purchaseParagrapgh)
 function purchase (){
     console.log("Clicked")
+    purchaseParagrapgh.textContent = "Error : Something went wrong!"
 }
+
