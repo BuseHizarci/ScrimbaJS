47ef8c67a60d36c7de7b67c19ace797804614f98
Buse HIZARCI <bhzrc2705@gmail.com>
2022-08-10 22:06:19 +0100

    First JS

---
+++ JS1/JS1.js
@@ -0,0 +1,9 @@
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error".
+
+
+
+function purchase (){
+    console.log("Clicked")
+}
