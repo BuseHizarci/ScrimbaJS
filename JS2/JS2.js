795a2010260c31f5beb579155ce15c133a580167
Buse HIZARCI <bhzrc2705@gmail.com>
2022-08-10 22:49:23 +0100

    Add Function Added

--- JS2/JS2.js
+++ JS2/JS2.js
@@ -1,14 +1,18 @@
-let num1 = 8
+let num1 = 3
 let num2 = 2
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
 
-// Create four functions: add(), subtract(), divide(), multiply()
+// Create four functions: add(), subtrasct(), divide(), multiply()
 // Call the correct function when the user clicks on one of the buttons
 // Perform the given calculation using num1 and num2
 // Render the result of the calculation in the paragraph with id="sum-el"
 
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+let sumEl = document.getElementById("sum-el")
 
+function add() {
 
+sumEl.textContent= num1 + num2
+}
