458d550a2b73b200ad5bd23f225717f13a94e0dc
Buse HIZARCI <bhzrc2705@gmail.com>
2022-08-13 22:40:57 +0100

    Guest Score Function

--- JS3/JS3.js
+++ JS3/JS3.js
@@ -1,20 +1,19 @@
 let homeScoreBtnOne = document.getElementById("home-score-btn-1")
+// Birinci buttonu tanimlamadik let ile 
+// home-score-btn-1 u bul dedim ,
+
 let homeStoreEl =document.getElementById("home-score")
+
+// home score'a bunun icine yazdiracagim icin tanimlama yaptim
 let homeScore = 2
 
 function increaseHomeScoreOne(){
     homeScore += 1
     homeStoreEl.textContent = homeScore
 }
+// fonksiyon home scoreu bir artttiriyor daha once belirledigimiz homescore u kendimiz yazdik
+// Id si ile kontrol edebildigimiz homescore - store un icine fonksiyonda arttirdigimizi yaziyoruz.
 
-
-
-let homeScoreButtonTwo = document.getElementById("home-score-btn-2")
-
-function increaseHomeScoreTwo (){
-    homeScore += 2
-    homeStoreEl.textContent= homeScore
-}
 
 let GuestScoreBtnOne = document.getElementById("guest-home-btn-1")
 let GuestStoreEl = document.getElementById("guest-score")
