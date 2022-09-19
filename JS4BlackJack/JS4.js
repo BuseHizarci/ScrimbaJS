7019f4e6386d20967528eba4f1c10715765ae2e0
Buse HIZARCI <bhzrc2705@gmail.com>
2022-09-19 16:31:18 +0100

    20September

--- JS4BlackJack/JS4.js
+++ JS4BlackJack/JS4.js
@@ -1,12 +1,19 @@
 let firstCard = 10
+//Birinci kartim
 let secondCard = 4
+//Ikinci kartim
 // 1. Create a new array - cards - that contains firstCard and secondCard
 let cards = [firstCard, secondCard]
+//Kartlarimi listeledigim arrayim 
 let sum = firstCard + secondCard
+//Kartlarimin toplami
 let hasBlackJack = false
+//boolean blackjack var mi yok mu ?
 let isAlive = true
 let message = ""
+
 let messageEl = document.getElementById("message-el")
+// Mesajimin tutuldugu yer
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
@@ -31,8 +38,21 @@ function renderGame() {
 }
 
 
+
 function newCard() {
     let card = 6
     sum += card
+    cards.push(card)
+
     renderGame()
+}
+
+
+function renderGame(){
+cardsEl.textContent = " Cards " + cards[0] + cards [1]
+sumEl.textContent = "Sum" + sum 
+if (sum > 20 ) {
+    message = "Do you want to draw ?"
+    hasBlackJack = true
+}
 }
