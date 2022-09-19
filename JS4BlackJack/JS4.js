46aa217911cc063d91d8a38a54c58a4451009fff
Buse HIZARCI <bhzrc2705@gmail.com>
2022-09-19 20:41:26 +0100

    math floor

--- JS4BlackJack/JS4.js
+++ JS4BlackJack/JS4.js
@@ -1,29 +1,27 @@
 let firstCard = 10
-//Birinci kartim
 let secondCard = 4
-//Ikinci kartim
-// 1. Create a new array - cards - that contains firstCard and secondCard
 let cards = [firstCard, secondCard]
-//Kartlarimi listeledigim arrayim 
 let sum = firstCard + secondCard
-//Kartlarimin toplami
 let hasBlackJack = false
-//boolean blackjack var mi yok mu ?
 let isAlive = true
 let message = ""
-
 let messageEl = document.getElementById("message-el")
-// Mesajimin tutuldugu yer
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+
+// Create a function, getRandomCard(), that always returns the number 5
+
 
 function startGame() {
     renderGame()
 }
 
 function renderGame() {
-    // 2. Refer to the cards array when rendering out the cards
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -38,21 +36,10 @@ function renderGame() {
 }
 
 
-
 function newCard() {
     let card = 6
     sum += card
     cards.push(card)
-
+    console.log(cards)
     renderGame()
-}
-
-
-function renderGame(){
-cardsEl.textContent = " Cards " + cards[0] + cards [1]
-sumEl.textContent = "Sum" + sum 
-if (sum > 20 ) {
-    message = "Do you want to draw ?"
-    hasBlackJack = true
-}
 }
