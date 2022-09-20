6f16663e45dcf9b440994836335493ef530bdfd3
Buse HIZARCI <bhzrc2705@gmail.com>
2022-09-20 20:55:10 +0100

    notes about blackjack

--- JS4BlackJack/JS4.js
+++ JS4BlackJack/JS4.js
@@ -1,18 +1,20 @@
 let firstCard = getRandomCard()
+/* Random bir card cagirma ve bu card mat.floor ile yuvarlanmis ve 13 tane 13 e kadar */
 let secondCard = getRandomCard()
 let cards = [firstCard, secondCard]
+/* Bir arrayim var icinde iki kart listeliyorum */
 let sum = firstCard + secondCard
 let hasBlackJack = false
+/* bu boolean bir ifade ve eger blackjackim varsa bu dogru ise true oluyor */
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
+
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 
 function getRandomCard() {
-    // if 1     -> return 11
-    // if 11-13 -> return 10
     let randomNumer = Math.floor( Math.random()*13 ) + 1
     if (randomNumer > 10) {
         return 10
